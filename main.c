d063ca548dfddc4e151a8a9a119c21acb902a302
VitalyArtemiev <vitaly.artemiev@yandex.ru>
2022-05-05 16:18:00 +0300

    Bizarre bug

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <signal.h>
+#include <strings.h>
 
 #include <unistd.h>
 #include <netinet/in.h>
@@ -15,7 +16,35 @@
 #define HELPSTRING "Usage: cdemo -p [port]\n"
 #define SA struct sockaddr
 
-long getport(int argc, char *argv[]) {
+int sockfd;
+
+struct ev_loop *main_loop;
+struct ev_loop *conn_loop;
+
+enum Msg {msg_exit, msg_port, msg_in, msg_out};
+
+struct ev_port {
+    enum Msg msg;
+    long port;
+};
+
+struct ev_exit {
+    enum Msg msg;
+};
+
+struct ev_in_out {
+    enum Msg msg;
+    char *str;
+    int connfd;
+};
+
+union ev_msg {
+    struct ev_port p;
+    struct ev_exit e;
+    struct ev_in_out io;
+};
+
+long get_port(int argc, char *argv[]) {
     if (argc < 3) {
         printf(HELPSTRING);
         exit(0);
@@ -42,59 +71,10 @@ long getport(int argc, char *argv[]) {
     }
 }
 
-#define MAX_MSG_LEN 80
-
-void echo(int connfd)
-{
-    char buff[MAX_MSG_LEN];
-    for (;;) {
-        bzero(buff, MAX_MSG_LEN);
-
-        read(connfd, buff, sizeof(buff));
-
-        printf("From client: %s\n", buff);
-
-        errno = 0;
-        write(connfd, buff, sizeof(buff));
-        if (errno != 0) {
-            printf("Connection closed\n");
-            break;
-        }
-
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server received shutdown command\n");
-            break;
-        }
-    }
-}
-
-struct sockparams {
-    int len;
-    int sockfd;
-};
-
-void* handleconnection(struct sockparams* sp) {
-    struct sockaddr_in client;
-
-    int connfd = accept(sp->sockfd, (SA*)&client, &(sp->len));
-    if (connfd < 0) {
-        printf("Accept failed\n");
-        exit(0);
-    }
-    else
-        printf("Connection accepted\n");
-
-    echo(connfd);
-}
-
-int main(int argc, char *argv[]) {
-    signal(SIGPIPE,SIG_IGN);
-    long port = getport(argc, argv);
-
-    int sockfd;
+int setup_sock(long port) {
     struct sockaddr_in servaddr;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
     if (sockfd == -1) {
         printf("Socket creation failed\n");
         exit(0);
@@ -125,17 +105,214 @@ int main(int argc, char *argv[]) {
     else
         printf("Server listening\n");
 
-    pthread_t id;
-    struct sockparams sp;
-    sp.sockfd = sockfd;
-    sp.len = sizeof(servaddr);
+    return sockfd;
+}
 
-    pthread_create(&id, NULL, handleconnection, &sp);
+#define MAX_MSG_LEN 80
 
+static void
+stdin_cb (EV_P_ ev_io *w, int revents)
+{
+    char buff[MAX_MSG_LEN];
+    for (;;) {
+        bzero(buff, MAX_MSG_LEN);
+
+        read(w->fd, buff, sizeof(buff));
+
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server received shutdown command\n");
+
+            ev_async signal;
+            union ev_msg m;
+            m.e.msg = msg_exit;
+            signal.data = &m;
+
+            printf("Sending msg_exit to main loop\n");
+
+            ev_async_send(main_loop, &signal);
+            break;
+        } else if (strncmp("port", buff, 4) == 0) {
+            printf("Changing port\n");
+
+            const char *nptr = &buff[5];
+            char *endptr = NULL;
+            errno = 0;
+            long port =  strtol(nptr, &endptr, 10);
+            if (nptr == endptr)
+                errno = 1;
+            else if (errno == 0 && nptr && *endptr != 0)
+                errno = 1;
+
+            if (errno != 0 || port <= 0) {
+                printf("Invalid port value\n");
+                exit(0);
+            }
+
+            ev_async signal;
+            union ev_msg m;
+            m.p.msg = msg_port;
+            m.p.port = port;
+            signal.data = &m;
+
+            printf("Sending msg_port to main loop\n");
+            ev_async_send(main_loop, &signal);
+            break;
+        }
+    }
+}
+
+void* handle_cli(){
+    struct ev_loop *loop = EV_DEFAULT;
+    ev_io stdin_watcher;
+
+    // initialise an io watcher, then start it
+    // this one will watch for stdin to become readable
+    ev_io_init (&stdin_watcher, stdin_cb, /*STDIN_FILENO*/ 0, EV_READ);
+    ev_io_start (loop, &stdin_watcher);
+
+    // now wait for events to arrive
+    ev_run (loop, 0);
+}
+
+static void cb_accept(EV_P_ ev_io *w, int revents) {
+    struct sockaddr_in client;
+
+    int len = sizeof(client);
+
+    int connfd = accept(w->fd, (SA*)&client, &len);
+    if (connfd < 0) {
+        printf("Accept failed\n");
+        exit(0);
+    }
+    else
+        printf("Connection accepted\n");
+
+    char* buff = malloc(MAX_MSG_LEN);
+    bzero(buff, MAX_MSG_LEN);
+    read(connfd, buff, sizeof(buff));
+
+    ev_async signal;
+    union ev_msg m;
+    m.io.msg = msg_in;
+    m.io.str = buff;
+    m.io.connfd = connfd;
+    signal.data = &m;
+
+    printf("Sending msg_in to main loop\n");
+    ev_async_send(main_loop, &signal);
+}
+
+static void cb_respond(EV_P_ ev_io *w, int revents) {
+    union ev_msg m = *(union ev_msg*) w->data;
+
+    switch (m.io.msg) {
+        case msg_exit:
+            ev_break (EV_A_ EVBREAK_ONE);
+            break;
+        case msg_port:
+            //need to restart thread
+            ev_break (EV_A_ EVBREAK_ONE);
+            break;
+        case msg_in:
+            printf("Invalid msg in cb_respond\n");
+            break;
+        case msg_out:
+            errno = 0;
+            write(m.io.connfd, m.io.str, MAX_MSG_LEN);
+            if (errno != 0) {
+                printf("Connection closed\n");
+                break;
+            }
+    }
+}
+
+pthread_t connthread;
+
+void* handle_connection(int sockfd) {
+    conn_loop = EV_DEFAULT;
+
+    //watch for incoming connections and pass them to main thread
+    ev_io socket_watcher;
+    ev_io_init(&socket_watcher, cb_accept, sockfd, EV_READ);
+    ev_io_start(conn_loop, &socket_watcher);
+
+    ev_async msg_watcher;
+    ev_async_init(&msg_watcher, cb_respond);
+    ev_io_start(conn_loop, &msg_watcher);
+
+    ev_run(conn_loop, 0);
+}
+
+static void cb_msg(EV_P_ ev_async *w, int revents) {
+    union ev_msg m = *(union ev_msg*) w->data;
+    switch (m.io.msg) {
+        case msg_exit:
+            ev_break(EV_A_ EVBREAK_ONE);
+            break;
+        case msg_port: {
+            ev_async signal;
+            signal.data = &m;
+
+            printf("Sending msg_port to conn loop\n");
+            ev_async_send(conn_loop, &signal);
+
+            sockfd = setup_sock(m.p.port);
+            pthread_create(&connthread, NULL, handle_connection, &sockfd);
+        }
+            break;
+        case msg_in: {
+            char* buff = m.io.str;
+            int last;
+            for (last = MAX_MSG_LEN - 1; last > 1 ; last--) {
+                if (buff[last] != 0) {
+                    break;
+                }
+            }
+
+            for (int i = 0; i < last / 2; i++) {
+                char temp = buff[last - i];
+                buff[i] = buff[last - i];
+                buff[last - i] = temp;
+            }
+
+            ev_async signal;
+            union ev_msg mo;
+            mo.io.msg = msg_out;
+            mo.io.str = buff;
+            mo.io.connfd = m.io.connfd;
+            signal.data = &mo;
+
+            printf("Sending msg_out to conn loop\n");
+            ev_async_send(conn_loop, &signal);
+        }
+            break;
+        case msg_out:
+            printf("Invalid msg in cb_msg\n");
+            break;
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    signal(SIGPIPE,SIG_IGN);
+    long port = get_port(argc, argv);
+
+    sockfd = setup_sock(port);
+    printf("Socket %i", sockfd);
+
+    main_loop = EV_DEFAULT;
+    ev_async msg_watcher;
+    ev_async_init(&msg_watcher, cb_msg);
+    ev_async_start(main_loop, &msg_watcher);
+
+    pthread_t clithread;
+    pthread_create(&clithread, NULL, handle_cli, NULL);
+
+    pthread_create(&connthread, NULL, handle_connection, &sockfd);
+
+    ev_run(main_loop, 0);
 
     close(sockfd);
-
-
 
     return 0;
 }
